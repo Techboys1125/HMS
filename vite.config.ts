d57dee63330c8678ec6da5c382f3129530fb8ed8
Rithick Nathan.R <115696016+Techboys1125@users.noreply.github.com>
2026-09-03 12:41:57 +0530

    Clean up comments in vite.config.ts
    Removed commented URLs from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 //http://192.168.1.44:8081
 //https://api.hms.viyaninfo.com
 //https://safe-hands-hms-backend.onrender.com
